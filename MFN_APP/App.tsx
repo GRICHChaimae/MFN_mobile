4a09a5717e99b79a95d7513a64052846f36feac4
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-03-06 14:55:20 +0100

    setup the architecture and add bottom bar

---
+++ MFN_APP/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomBar from './src/global/BottomeBar';
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <BottomBar />
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ MFN_APP/src/global/BottomeBar.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import ListTransitaires from '../screens/ListTransitaires';
+import Login from '../screens/transitaire/Login';
+import Register from '../screens/transitaire/Register';
+
+interface TabBarButtonProps {
+  label: string;
+  selected: boolean;
+  onPress: () => void;
+}
+
+const TabBarButton: React.FC<TabBarButtonProps> = ({ label, selected, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.tabButton}>
+      <Text style={[styles.tabLabel, selected && styles.selectedTabLabel]}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const BottomBar = () => {
+  const [selectedTab, setSelectedTab] = useState('Home');
+
+  return (
+    <View style={styles.container}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarStyle: [
+              { display: 'flex' },
+              null
+            ],
+            tabBarButton: (props) => (
+              <TabBarButton
+                label={route.name}
+                selected={selectedTab === route.name}
+                onPress={() => setSelectedTab(route.name)}
+                {...props}
+              />
+            ),
+          })}
+      >
+        <Tab.Screen options={{headerShown: false}} name="Home" component={Home} />
+        <Tab.Screen options={{headerShown: false, tabBarIcon: () => null}} name="Transitaires" component={ListTransitaires} />
+        <Tab.Screen options={{headerShown: false, tabBarIcon: () => null}} name="Register" component={Register} />
+        <Tab.Screen options={{headerShown: false}}  name="Login" component={Login} />
+      </Tab.Navigator>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+    },
+    tabBar: {
+      borderTopWidth: 1,
+      borderTopColor: '#ddd',
+    },
+    tabButton: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    tabLabel: {
+      color: '#333',
+      fontSize: 16,
+    },
+    selectedTabLabel: {
+      color: '#007aff',
+      fontWeight: 'bold',
+    },
+    screen: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export default BottomBar;
